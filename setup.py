aaca2f45b66da37d9f01c4e92cc97ca1956d40b8
Yu-Xiang Wang <yuxiangw@users.noreply.github.com>
2023-11-29 13:41:39 -0800

    Adding dependency to setup.py directly instead of loading a file.

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ def _parse_requirements(path):
     deps = []
     for line in f:
       deps.append(line.rstrip())
-    return deps
+    return depss
 
 setup(
    name='autodp',
-   version='0.2.3',
+   version='0.2.3.1',
    description='Automating Differential Privacy Computation',
    license="Apache",
    long_description="The package helps researchers and developers to correctly use advanced methods in differential privacy and obtain provable DP guarantees. The core of the package is an analytical moments accountant that keeps track of Renyi Differential Privacy in analytical forms.",
@@ -28,7 +28,12 @@ setup(
    download_url = 'https://github.com/yuxiangw/autodp/archive/refs/tags/v0.2.3.tar.gz',   
    keywords = ['Differential Privacy','Moments Accountant','Renyi Differential Privacy'],
    packages=['autodp'],  #same as name
-   install_requires=[_parse_requirements('requirements.txt')], #external packages as dependencies
+   install_requires=[
+     'absl-py',
+     'matplotlib',
+     'numpy>=1.18',
+     'scipy>=1.2.1'
+   ], #external packages as dependencies
    classifiers=['Development Status :: 3 - Alpha',
    'Intended Audience :: Developers',
    'Intended Audience :: Science/Research',
